1e7e606e5b39e6038bb3ba5536e073b8d520f68a
istenno <hhgrant7@gmail.com>
2014-05-10 21:40:05 -0700

    button styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   # before_filter :authenticate_user!
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+  	rx_alerts_path
+  end
 end
