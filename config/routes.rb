e54b9bbd47d0a6768cce9164b55c4007ecd7dbd9
Zachary Moshansky <zmoshansky@gmail.com>
2014-05-11 11:56:17 -0700

    Fixed minor render bug

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ Checkrx::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'rx_alerts', action: :index
 
   # See how all your routes lay out with "rake routes"
 
